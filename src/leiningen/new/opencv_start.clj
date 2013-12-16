3aae3a6c76046fa53553cb049dac751204610b58
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-12-16 11:06:17 +0100

    update content

--- src/leiningen/new/opencv_start.clj
+++ src/leiningen/new/opencv_start.clj
@@ -1,14 +1,32 @@
 (ns leiningen.new.opencv-start
-  (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
+  (:require [leiningen.new.templates :refer [renderer
+                                             year
+                                             name-to-path
+                                             sanitize-ns
+                                             multi-segment
+                                             ->files]]
             [leiningen.core.main :as main]))
 
-(def render (renderer "opencv-start"))
-
 (defn opencv-start
-  "FIXME: write documentation"
+  "A general project template for OpenCV
+
+Accept a group-id in the project name: lein new opencv-start foo.bar/baz"
   [name]
-  (let [data {:name name
-              :sanitized (name-to-path name)}]
+  (let [render (renderer "opencv-start")
+        main-ns (multi-segment (sanitize-ns name))
+        data {:raw-name name
+              :name (project-name name)
+              :namespace main-ns
+              :nested-dirs (name-to-path main-ns)
+              :year (year)}]
     (main/info "Generating fresh 'lein new' opencv-start project.")
     (->files data
-             ["src/{{sanitized}}/foo.clj" (render "foo.clj" data)])))
+             ["project.clj" (render "project.clj" data)]
+             ["profiles.clj" (render "profiles.clj" data)]
+             ["README.md" (render "README.md" data)]
+             ["doc/intro.md" (render "intro.md" data)]
+             [".gitignore" (render "gitignore" data)]
+             ["src/clj/{{nested-dirs}}.clj" (render "core.clj" data)]
+             ["test/clj/{{nested-dirs}}_test.clj" (render "test.clj" data)]
+             ["LICENSE" (render "LICENSE" data)]
+             "resources/images")))
