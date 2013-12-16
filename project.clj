f088b7a0fd62777d998628e04f08d404c0d0a3fa
magomimmo <mimmo.cosenza@gmail.com>
2013-12-16 09:33:45 +0100

    add pom-addition and change license to BSD

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject opencv-start/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :pom-addition [:developers [:developer
+                              [:id "magomimmo"]
+                              [:name "Mimmo Cosenza"]
+                              [:url "https://github.com/magomimmo"]
+                              [:email "mimmo.cosenza@gmail.com"]
+                              [:timezone "+1"]]]
+  :description "A Lein template to create generic OpenCV project"
+  :url "https://github.com/magomimmo/openvc-start"
+  :license {:name "The BSD 3-Clause License"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
   :eval-in-leiningen true)
